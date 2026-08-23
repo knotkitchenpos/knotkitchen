4190e40aea125a1c4e7cf43e546488749762bb40
AsmitGhosh10 <AsmitGhosh10@users.noreply.github.com>
2026-08-23 09:24:58 +0530

    fix: import useEffect in ManageMenu.jsx

--- pos-frontend/src/components/dashboard/ManageMenu.jsx
+++ pos-frontend/src/components/dashboard/ManageMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { enqueueSnackbar } from "notistack";
 import {
